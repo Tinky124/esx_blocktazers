385c191e51fd5e2bd18a1f01f44bac2d3365829e
Vyast <67253117+Vyast@users.noreply.github.com>
2021-03-24 00:18:07 -0600

    will now print in console when a player is kicked

--- server.lua
+++ server.lua
@@ -3,27 +3,35 @@ ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
 local allowedJobs = {
-    'police'
+    'police',
+    'offpolice'
 }
 
-local kickMessage = 'Stop trying to taze other players!'
+local kickMessage = 'Unauthorized Tazed Detected'
 
 AddEventHandler('weaponDamageEvent', function(sender, data)
     local allowed = false
     if sender ~= nil then
-        if data.damageType ~= 0 then
-            if data.weaponDamage == 1 then
-                local xPlayer = ESX.GetPlayerFromId(sender)
-                if xPlayer ~= nil then
-                    for k, v in pairs(allowedJobs) do
-                        if xPlayer.job ~= nil and xPlayer.job.name == v then 
-                            allowed = true
-                            break
+        local senderName = GetPlayerName(sender)
+        if senderName ~= nil then
+            if data.damageType ~= 0 then
+                if data.weaponDamage == 1 then
+                    local xPlayer = ESX.GetPlayerFromId(sender)
+                    if xPlayer ~= nil then
+                        local job, jobName = xPlayer.job, xPlayer.job.name
+                        if job ~= nil and jobName ~= nil then
+                            for k, v in pairs(allowedJobs) do
+                                if jobName == v then 
+                                    allowed = true
+                                    break
+                                end
+                            end
+                            if not allowed then 
+                                CancelEvent()
+                                print('^1'..senderName..'^0 (^1'..tonumber(sender)..'^0) has just been kicked for unauthorized blips!')
+                                DropPlayer(sender, kickMessage)
+                            end
                         end
-                    end
-                    if not allowed then 
-                        CancelEvent()
-                        DropPlayer(sender, kickMessage)
                     end
                 end
             end
