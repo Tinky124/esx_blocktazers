0206c5f060b98f0bbc1648eb8aea952d1347e13b
Vyast <67253117+Vyast@users.noreply.github.com>
2021-08-29 01:42:17 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,7 +2,7 @@ fx_version 'adamant'
 game 'gta5'
 
 author 'Vyast#0001'
-
 description 'Simple & optimized ESX resource that will stop modders from tazing other players'
+version '1.0.0'
 
 server_script 'server.lua'
