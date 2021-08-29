3d115497159e793914952f969cf76dd853df4745
Vyast <67253117+Vyast@users.noreply.github.com>
2021-08-29 01:42:04 -0600

    Update server.lua

--- server.lua
+++ server.lua
@@ -2,37 +2,30 @@ ESX = nil
 
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 
-local allowedJobs = {
-    'police',
-    'offpolice'
-}
+local whitelisted_jobs = {'police', 'offpolice'}
 
-local kickMessage = 'Unauthorized Tazed Detected'
+local kick_message = 'Unauthorized Taze Detected!'
 
 AddEventHandler('weaponDamageEvent', function(sender, data)
-    local allowed = false
-    if sender ~= nil then
-        local senderName = GetPlayerName(sender)
-        if senderName ~= nil then
-            if data.damageType ~= 0 then
-                if data.weaponDamage == 1 then
-                    local xPlayer = ESX.GetPlayerFromId(sender)
-                    if xPlayer ~= nil then
-                        local job, jobName = xPlayer.job, xPlayer.job.name
-                        if job ~= nil and jobName ~= nil then
-                            for k, v in pairs(allowedJobs) do
-                                if jobName == v then 
-                                    allowed = true
-                                    break
-                                end
-                            end
-                            if not allowed then 
-                                CancelEvent()
-                                print('^1'..senderName..'^0 (^1'..tonumber(sender)..'^0) has just been kicked for an unauthorized taze against another player!')
-                                DropPlayer(sender, kickMessage)
-                            end
-                        end
+    local src = sender
+    local src_name = GetPlayerName(src)
+    if src_name ~= nil then
+        if data.weaponType == 911657153 then --'WEAPON_STUNGUN'
+            local xPlayer = ESX.GetPlayerFromId(src)
+            if xPlayer ~= nil and xPlayer.job ~= nil then
+                local allowed = false
+
+                for k, v in pairs(whitelisted_jobs) do
+                    if xPlayer.job.name == v then 
+                        allowed = true
+                        break
                     end
+                end
+
+                if not allowed then 
+                    CancelEvent()
+                    print('^1'..src_name..'^0 (ID: ^1'..src..'^0) has just been kicked for an unauthorized taze against another player!')
+                    DropPlayer(src, kickMessage)
                 end
             end
         end
