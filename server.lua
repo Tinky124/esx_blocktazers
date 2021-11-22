39b21538a2fc307fe2374ea313a5cc9572b9e021
Tinky <48660658+Tinky124@users.noreply.github.com>
2021-11-22 14:17:15 +0100

    little update :)

--- server.lua
+++ server.lua
@@ -2,7 +2,7 @@ local kick_message = 'Unauthorized Taze Detected!'
 AddEventHandler('weaponDamageEvent', function(sender, data)
     local src = sender
     local src_name = GetPlayerName(src)
-    if src_name ~= nil then
+    if not src_name then
         if data.weaponType == 911657153 then --'WEAPON_STUNGUN'
             if not GetSelectedPedWeapon(GetPlayerPed(src)) == 911657153 then
                 CancelEvent()
